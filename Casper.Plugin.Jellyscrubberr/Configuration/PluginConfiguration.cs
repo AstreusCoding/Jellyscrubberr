84881f611923d780eec815bf4d64498927742a3a
CasperDoesCoding <pandaa.casper@gmail.com>
2024-03-31 00:51:57 +0100

    Update PluginConfiguration.cs
    changed default values

--- Casper.Plugin.Jellyscrubberr/Configuration/PluginConfiguration.cs
+++ Casper.Plugin.Jellyscrubberr/Configuration/PluginConfiguration.cs
@@ -5,19 +5,19 @@ namespace Casper.Plugin.Jellyscrubberr.Configuration;
 
 public class PluginConfiguration : BasePluginConfiguration
 {
-    public FileSaveLocation fileSaveLocation { get; set; } = FileSaveLocation.SameFolder;
+    public FileSaveLocation fileSaveLocation { get; set; } = FileSaveLocation.CustomFolder;
     public string customFolderName { get; set; } = "trickplay";
 
     public HwAccelerationOptions hardwareAcceleration { get; set; } = HwAccelerationOptions.None;
 
-    public ProcessPriorityClass processPriority { get; set; } = ProcessPriorityClass.High;
+    public ProcessPriorityClass processPriority { get; set; } = ProcessPriorityClass.Normal;
 
-    public bool OnDemandGeneration { get; set; } = false;
-    public bool extractionDuringLibraryScan { get; set; } = true;
-    public bool LocalMediaFolderSaving { get; set; } = true;
+    public bool OnDemandGeneration { get; set; } = true;
+    public bool extractionDuringLibraryScan { get; set; } = false;
+    public bool LocalMediaFolderSaving { get; set; } = false;
     public bool shouldRegenerateIfOldManifest { get; set; } = true;
 
-    public int qScaleInput { get; set; } = 0;
+    public int qScaleInput { get; set; } = 4;
     public int processThreads { get; set; } = 1;
     public int imageInterval { get; set; } = 10000;
     public int imageWidthResolution { get; set; } = 320;
