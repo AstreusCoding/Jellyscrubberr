e793b1ed5b2d6dff8595c6f266b3318f051880b4
CasperDoesCoding <pandaa.casper@gmail.com>
2024-03-30 22:26:46 +0100

    Added another setting.
    Added option to not regenerate manifest files / bif files not made by Jellyscrubberr

--- Casper.Plugin.Jellyscrubberr/Configuration/PluginConfiguration.cs
+++ Casper.Plugin.Jellyscrubberr/Configuration/PluginConfiguration.cs
@@ -5,24 +5,22 @@ namespace Casper.Plugin.Jellyscrubberr.Configuration;
 
 public class PluginConfiguration : BasePluginConfiguration
 {
-    public bool LocalMediaFolderSaving { get; set; } = true;
-
     public FileSaveLocation fileSaveLocation { get; set; } = FileSaveLocation.SameFolder;
     public string customFolderName { get; set; } = "trickplay";
 
     public HwAccelerationOptions hardwareAcceleration { get; set; } = HwAccelerationOptions.None;
 
     public ProcessPriorityClass processPriority { get; set; } = ProcessPriorityClass.High;
-    public int processThreads { get; set; } = 1;
-
-    public int imageInterval { get; set; } = 10000;
-
-    public int imageWidthResolution { get; set; } = 320;
 
     public bool OnDemandGeneration { get; set; } = false;
+    public bool extractionDuringLibraryScan { get; set; } = true;
+    public bool LocalMediaFolderSaving { get; set; } = true;
+    public bool shouldRegenerateIfOldManifest { get; set; } = true;
 
     public int qScaleInput { get; set; } = 0;
+    public int processThreads { get; set; } = 1;
+    public int imageInterval { get; set; } = 10000;
+    public int imageWidthResolution { get; set; } = 320;
 
-    public bool extractionDuringLibraryScan { get; set; } = true;
     public MetadataScanBehaviour ScanBehavior { get; set; } = MetadataScanBehaviour.NonBlocking;
 }
